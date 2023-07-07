0be3a5ea2b84f3cd2f3a9c40ce951bde8a6ffb26
Induwara Kandapahala <kandapah@ualberta.ca>
2023-07-06 19:49:34 -0600

    Initial Commit

---
+++ WeConvert/WeConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeConvertApp.swift
+//  WeConvert
+//
+//  Created by Induwara on 2023-07-06.
+//
+
+import SwiftUI
+
+@main
+struct WeConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
